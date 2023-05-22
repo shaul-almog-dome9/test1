d30e7ac2ee26eec466d128b8d385b17b8ebaf507
Shaul Almog <shaula@checkpoint.com>
2023-05-22 19:00:47 +0300

    Update test-me.js

--- test-me.js
+++ test-me.js
@@ -1,2 +1,22 @@
 
 console.log('test');
+function fibonacci(num) {
+    if (num <= 1) {
+        return 1;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+const createFile = (fileName, content) => {
+  fs.writeFile(fileName, content, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('File created');
+  });
+};
+
+createFile('hello.txt', 'Hello World');
+
+console.log(fibonacci(5));
